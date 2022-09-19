d1e3bfb292f5f6677eda8715397ad77504566c03
Chung <qchung582@gmail.com>
2022-09-19 23:44:08 +0700

    changes

--- exercise3/script.js
+++ exercise3/script.js
@@ -1,7 +1,6 @@
-function dienTich (a, b, c, d){
+function dienTich (a, b, c,){
     let p = (a + b + c)/2;
-    console.log(d)
     return Math.sqrt(p * (p - a) * (p - b) * (p - c));
 }
-d = 3
-console.log(dienTich(3,4,5, d))
+
+console.log(dienTich(3, 4, 5))
