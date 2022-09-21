d2ae5f572c25d0c2e079748163fe300e0d57e1b8
m8nhchau <77391385+m8nhchau@users.noreply.github.com>
2022-09-21 23:15:37 +0700

    exercise3

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-function print_current_page()
-{
-window.print();
-}
+let today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth()+1; 
+let yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd='0'+dd;
+} 
+
+if(mm<10) {
+    mm='0'+mm;
+} 
+
+
+console.log(mm + '-' + dd + '-' + yyyy);
+console.log(mm + '/' + dd + '/' + yyyy);
+console.log(dd + '-' + mm + '-' + yyyy);
+console.log(dd + '/' + mm + '/'  +yyyy);
