933d1be052c3214af47ad10c9deb483cccb312d3
Chung <qchung582@gmail.com>
2022-09-22 00:11:27 +0700

    changes

--- exercise5/script.js
+++ exercise5/script.js
@@ -2,7 +2,7 @@ let myLet = setInterval(myFunction, 1000);
 let text = 'w3resource';
 let newArray = text.split('')
 
-function myFunction(){
+function myFunction() {
     let letter = newArray.pop();
     let newLetter = newArray.unshift(letter);
     let newText = newArray.join('')
--- exercise6/script.js
+++ exercise6/script.js
@@ -1,13 +1,13 @@
-function leapYear (year){
-    if(year % 100 ==0){
-        if(year % 400==0){
+function leapYear(year) {
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
             console.log('Năm nhuận');
-        }else{
+        } else {
             console.log('Không phải năm nhuận');
         }
-        
+
     }
-    else if (year % 4==0){
+    else if (year % 4 == 0) {
         console.log('Năm nhuận');
     }
     else {
