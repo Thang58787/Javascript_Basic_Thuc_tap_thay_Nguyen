34da8ea520f4af7ddf489bb498fda53392c5714e
Chung <qchung582@gmail.com>
2022-09-22 15:13:10 +0700

    exercise8

---
+++ exercise8/script.js
@@ -0,0 +1,11 @@
+function ramDOm(n) {
+    let number = Math.floor(Math.random() * 10) + 1;
+    if (n === number) {
+        console.log('Good Work');
+    }
+    else {
+        console.log('Not matched');
+    }
+}
+console.log(ramDOm(4));
+prompt('nhapso')
