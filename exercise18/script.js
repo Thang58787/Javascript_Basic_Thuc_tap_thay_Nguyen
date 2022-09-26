2be3302fd529cfb6cbd7e96c18ca92ba36241c51
thang5878 <thang5878@gmail.com>
2022-09-26 10:01:19 +0700

    chore(exercise18): format code

--- exercise18/script.js
+++ exercise18/script.js
@@ -1,12 +1,12 @@
 function check(num1, num2) {
-    if((num1===50 || num2===50)||(num1+num2===50)) {
+    if ((num1 === 50 || num2 === 50) || (num1 + num2 === 50)) {
         return true;
-    }else {
+    } else {
         return false;
     }
 }
 
-console.log(check(50,50));
-console.log(check(20,50));
-console.log(check(20,20));
-console.log(check(20,30));
+console.log(check(50, 50));
+console.log(check(20, 50));
+console.log(check(20, 20));
+console.log(check(20, 30));
