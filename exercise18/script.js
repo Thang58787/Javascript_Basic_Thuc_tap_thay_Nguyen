ee2addc7a83810ebeec49639af097a8ade859b4a
thang5878 <thang5878@gmail.com>
2022-09-26 10:00:21 +0700

    feat: add exercise18 folder

---
+++ exercise18/script.js
@@ -0,0 +1,12 @@
+function check(num1, num2) {
+    if((num1===50 || num2===50)||(num1+num2===50)) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+console.log(check(50,50));
+console.log(check(20,50));
+console.log(check(20,20));
+console.log(check(20,30));
