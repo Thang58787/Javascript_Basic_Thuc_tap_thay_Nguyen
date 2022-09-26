184a933a10a012892e4a80c38aa303b7ffe01066
Chung <qchung582@gmail.com>
2022-09-26 18:45:40 +0700

    exercise23

--- exercise23/script.js
+++ exercise23/script.js
@@ -1,8 +1,12 @@
-function changingPosition (str){
+function changingPosition(str) {
     str = str.split('');
-    temp = str[0];
-    str[0] = str[str.length - 1];
-    str[str.length - 1]=temp;
-    return str.join('')
+    if (str.length <= 1) {
+        return str.join('')
+    } else {
+        temp = str[0];
+        str[0] = str[str.length - 1];
+        str[str.length - 1] = temp;
+        return str.join('')
+    }
 }
-console.log(changingPosition('quangchung'))
+console.log(changingPosition('q'))
