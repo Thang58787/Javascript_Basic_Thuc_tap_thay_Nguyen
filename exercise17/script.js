c84383d86fae2b1c6af842dcd6a0a5d46f12757f
thang5878 <thang5878@gmail.com>
2022-09-26 09:54:21 +0700

    feat: add exercise17 folder

---
+++ exercise17/script.js
@@ -0,0 +1,11 @@
+function difference(num) {
+    if(num > 19) {
+        return (num -19)*3;
+    } else {
+        return 19-num;
+    }
+}
+
+console.log(difference(12));
+console.log(difference(19));
+console.log(difference(22));
