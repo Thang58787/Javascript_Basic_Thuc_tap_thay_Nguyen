a5a1e89db1dca0341d19d5653a48c3e60488759e
m8nhchau <chauth247@gmail.com>
2022-09-27 23:16:04 +0700

    exjvscript

---
+++ exercise15/main.js
@@ -0,0 +1,9 @@
+function difference(n)//ham so sanh khac biet
+ {
+    if (n <= 13)
+        return 13 - n;
+    else
+        return (n - 13) * 2;
+ }
+console.log(difference(88))
+console.log(difference(77))
