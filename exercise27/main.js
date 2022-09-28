4cb4b8e9f9aa9cf1216c734568541d9cc027f535
m8nhchau <chauth247@gmail.com>
2022-09-28 23:32:48 +0700

    exjvscript

---
+++ exercise27/main.js
@@ -0,0 +1,10 @@
+function check(str){
+    if(str.substring(0,4)==="Java"){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  console.log(check("Javascript"));
+  console.log(check("html"));
