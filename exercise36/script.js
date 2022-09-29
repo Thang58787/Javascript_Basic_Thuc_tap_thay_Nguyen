53004867611e9702be607d399f2cec81c7ac3022
Chung <qchung582@gmail.com>
2022-09-29 15:28:29 +0700

    add exercise

---
+++ exercise36/script.js
@@ -0,0 +1,15 @@
+function checkLastdigit (a,b,c) {
+    const arrayOfDigitA = Array.from(String(a), Number);
+    const arrayOfDigitB = Array.from(String(b), Number);
+    const arrayOfDigitC = Array.from(String(c), Number);
+    let x = arrayOfDigitA.pop();
+    let y = arrayOfDigitB.pop();
+    let z = arrayOfDigitC.pop();
+    if(x==y && x==z && y==z){
+        return true;
+    }
+    return false;
+
+}
+console.log(checkLastdigit(40,50,60))
+console.log(checkLastdigit(40,50,61))
