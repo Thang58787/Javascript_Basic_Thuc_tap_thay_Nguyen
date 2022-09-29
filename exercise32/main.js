60749685dd9f2a370602bbd4f774bcff23260947
m8nhchau <chauth247@gmail.com>
2022-09-29 23:00:31 +0700

    exjvscript

---
+++ exercise32/main.js
@@ -0,0 +1,16 @@
+function nearest(a,b){
+    var a1=100-a
+    var b1=100-b
+
+    if(a1==b1){
+        return false;
+    }
+    if(a1>b1){
+      return b 
+    }else{
+      return a 
+    } 
+
+  }
+  console.log(nearest(90,90));
+  console.log(nearest(80,90));
