5d4deb20f84d150ac6327b209bb07d9b534c73bf
Nguyễn Ngọc Thắng <85829177+Thang58787@users.noreply.github.com>
2022-10-02 17:26:51 +0700

    fix code style

--- exercise40/script.js
+++ exercise40/script.js
@@ -1,12 +1,13 @@
 // Đây là bài 41, không phải bài 40
 function checkNumber(a, b, c) {
-    if (a == b && a == c && b == c) {
+    // Khi biết trong if chắc chắn có 1 lệnh thì không nên dùng ngoặc nhọn
+    if (a == b && a == c && b == c)
         return 30;
-    } if (a == b || a == c || b == c) {
+    if (a == b || a == c || b == c)
         return 40;
-    }
     return 20;
 }
+
 console.log(checkNumber(20, 10, 20));
 console.log(checkNumber(20, 20, 20));
 console.log(checkNumber(20, 21, 22));
