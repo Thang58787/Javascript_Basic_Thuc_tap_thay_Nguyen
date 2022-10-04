3e9c249967771a136b2f8c37b8bc451cc4c8c1ae
Chung <qchung582@gmail.com>
2022-10-04 10:32:58 +0700

    add exercise

--- exercise50/script.js
+++ exercise50/script.js
@@ -1,14 +1,14 @@
 function titleCase(str) {
 
     var myString = str.toLowerCase().split(' ');
-    // for (var i = 0; i < myString.length; i++) {
-    //     var subString = myString[i].split('');
-    //     // for (var j = 0; j < subString.length; j++) {
-    //     //     subString[0] = subString[0].toUpperCase();
-    //     // }
-    //     // myString[i] = subString.join('');
-    // }
+    for (var i = 0; i < myString.length; i++) {
+        var subString = myString[i].split('');
+        for (var j = 0; j < subString.length; j++) {
+            subString[0] = subString[0].toUpperCase();
+        }
+        myString[i] = subString.join('');
+    }
 
-    return myString
+    return myString.join(' ');
 }
-console.log(titleCase('QuAng'))
+console.log(titleCase('quang chung'))
