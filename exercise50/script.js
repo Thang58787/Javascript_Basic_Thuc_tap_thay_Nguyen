be6f7563d20b915af18957ff1243edbeb5e348a0
Chung <qchung582@gmail.com>
2022-10-04 10:31:38 +0700

    add exercise

---
+++ exercise50/script.js
@@ -0,0 +1,14 @@
+function titleCase(str) {
+
+    var myString = str.toLowerCase().split(' ');
+    // for (var i = 0; i < myString.length; i++) {
+    //     var subString = myString[i].split('');
+    //     // for (var j = 0; j < subString.length; j++) {
+    //     //     subString[0] = subString[0].toUpperCase();
+    //     // }
+    //     // myString[i] = subString.join('');
+    // }
+
+    return myString
+}
+console.log(titleCase('QuAng'))
