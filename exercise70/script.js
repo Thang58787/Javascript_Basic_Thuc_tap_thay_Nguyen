b08c5821cfd51adc575ef497d74ffb2ceabb0384
thang5878 <thang5878@gmail.com>
2022-10-06 13:03:56 +0700

    feat: add exercise70

---
+++ exercise70/script.js
@@ -0,0 +1,7 @@
+function rotateElementLeft(array)
+{
+    return [array[1], array[2], array[0]];
+}
+console.log(rotateElementLeft([3, 4, 5]));  
+console.log(rotateElementLeft([0, -1, 2]));  
+console.log(rotateElementLeft([7, 6, 5])); 
