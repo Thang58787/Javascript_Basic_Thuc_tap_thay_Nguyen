ce4fd3afa322fb115e9a70d429fcbb7be102944a
thang5878 <thang5878@gmail.com>
2022-10-07 11:18:27 +0700

    feat: add exercise73

---
+++ exercise73/script.js
@@ -0,0 +1,7 @@
+function reverseArray(array) {
+    return array.reverse();
+}
+
+console.log(reverseArray([5, 4, 3])); 
+console.log(reverseArray([1, 0, -1]));  
+console.log(reverseArray([2, 3, 1]));
