489ce17f375a7b3fc3e1f340c5e8d040cc104f66
Chung <qchung582@gmail.com>
2022-10-08 12:16:17 +0700

    add exercise

---
+++ exercise75/script.js
@@ -0,0 +1,6 @@
+function newArray(arr1, arr2) {
+    return [arr1[1], arr2[1]];
+}
+console.log(newArray([1, 2, 3], [1, 5, 6]));  
+console.log(newArray([3, 3, 3], [2, 8, 0]));  
+console.log(newArray([4, 2, 7], [2, 4, 5])); 
