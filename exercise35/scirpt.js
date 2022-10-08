6ef8ad7debb9246079a3717ae1ae3a87d1d911f1
m8nhchau <chauth247@gmail.com>
2022-10-08 22:04:47 +0700

    exjvscript

---
+++ exercise35/scirpt.js
@@ -0,0 +1,10 @@
+function specific(n,m){
+    console.log(n)
+    
+    if(n.substring(1,4)===m){
+      return true
+    }else{
+      return false;
+    }
+  }
+  console.log(specific("chau day","hau"))
