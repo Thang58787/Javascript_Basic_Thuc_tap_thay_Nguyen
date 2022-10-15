fed2bf234964908a481ad7e9420ea31869842f25
Chung <qchung582@gmail.com>
2022-10-15 14:14:06 +0700

    add exercise

---
+++ exercise94/script.js
@@ -0,0 +1,19 @@
+function appearsNUmber (arr){
+    arr.sort();
+    let result = 0
+    let max =0 ;
+    let temp = 0
+    for(let i =0;i<arr.length;i++){
+        if(arr[i]==arr[i+1]){
+            temp++;
+        } else{
+            temp =0;
+        }
+        if(temp>max){
+            result = arr[i];
+            max = temp;
+        }
+    }
+    return result
+}
+console.log(appearsNUmber([1, 2, 3, 2, 2, 8, 1, 9]))
