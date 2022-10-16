8e8f1b6a26227d2a62511b625853b20670ab6d72
Chung <qchung582@gmail.com>
2022-10-16 18:40:01 +0700

    add exercise

---
+++ exercise101/script.js
@@ -0,0 +1,16 @@
+function checkString(str) {
+    for (let i = 0; i < str.length - 1; i++) {
+        if ((/[a-z]/.test(str[i]) && /[a-z]/.test(str[i + 1]))) {
+            return false
+        }
+    }
+    for (let i = 0; i < str.length - 2; i++) {
+        if ((/[a-z]/.test(str[i]) && /[a-z]/.test(str[i + 2]))) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+console.log(checkString('xAr'));
+console.log(checkString('XXAx')); 
