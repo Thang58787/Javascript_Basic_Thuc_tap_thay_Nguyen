0cb0f98ff16235899cb25b22d11d5b34cd8765bd
Chung <qchung582@gmail.com>
2022-10-19 08:37:50 +0700

    add exercise

--- exercise_125/script.js
+++ exercise_125/script.js
@@ -1,12 +1,12 @@
 function makePalindrome (arr){
     let temp = 0;
-    let shortest = 0
+    let longest = 0
     for(let i =0;i<arr.length;i++){
-        if(arr[i].length<temp){
+        if(arr[i].length>temp){
             temp = arr[i].length;
-            shortest = arr[i]
+            longest = arr[i]
         }
     }
-    return shortest;
+    return longest;
 }
 console.log(makePalindrome(['avb','a','abcd']))
